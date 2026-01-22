8f44fb6d9fd99224ce6c17518ad3852c86bc1c68
KHarsh083 <harshkatiyar7373@gmail.com>
2026-01-22 10:35:08 +0530

    all done with import array

--- python-dsa/arrays/array1.py
+++ python-dsa/arrays/array1.py
@@ -38,11 +38,11 @@ val2 =array('d',[1,2,3,2.4,5,6,])
 
 # print("\n")
 
-val.insert(0,2)
-val.append(55)
-val[1]=22 #overwrite
-for i in val:
-    print(i, end=",")
+# val.insert(0,2)
+# val.append(55)
+# val[1]=22 #overwrite
+# for i in val:
+#     print(i, end=",")
 
 # coppyArray = array(val.typecode , (i for i in val )) #value of i in copyarray == value of in val
 # coppyArray = array(val.typecode , (i*3 for i in val ))
@@ -56,16 +56,24 @@ for i in val:
 print("\n")
 #slicing
 # a = araay_name[startindex : end index]
-abc = val[3:6] #also [0:-3] can happen meaning last 3 element are ot present
-for i in abc:
-    print(i, end=",")
-abcc =val[::-1] #reverse
-print("\n")
-for i in abcc:
-    print(i, end=",")
+# abc = val[3:6] #also [0:-3] can happen meaning last 3 element are ot present
+# for i in abc:
+#     print(i, end=",")
+# abcc =val[::-1] #reverse
+# print("\n")
+# for i in abcc:
+#     print(i, end=",")
 
 #user input
-n=int(input("how many elements"))
-input_array= array('i',[])
-for i in range(0,n):
-    input_array.append(int(input(f"enter the element {i} ")))
+# n=int(input("how many elements"))
+# input_array= array('i',[])
+# for i in range(0,n):
+#     input_array.append(int(input(f"enter the element {i} ")))
+
+# i=input_array.index(45)   #index of particular element
+# print(i)
+
+arr =array('i',[1,2,3,4,5,6])
+print(arr)
+for i in arr:
+    print (i,end='')
