01afb042ab66602aef216f278607b2690e09eb60
KHarsh083 <harshkatiyar7373@gmail.com>
2026-02-05 23:47:59 +0530

    day 6 and 7 fastapi

--- backend-python/fastapi/first.py
+++ backend-python/fastapi/first.py
@@ -4,7 +4,15 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data":{"name":"HARSH"}}
+    return {"data":"blog list"}
 @app.get("/about")
 def about():
     return{"data":{"quote":"Hello World"}}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data":"all unpublished blogs"}
+
+@app.get("/blog/{id}")
+def show(id:int):
+    return {"data":id}
